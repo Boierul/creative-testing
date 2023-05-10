a67c8cc1311c69a9088f660ebc7e4f7b23a02ec9
boierul <theboierul@gmail.com>
2023-05-10 23:11:42 +0200

    ...

---
+++ loaders/hacker-loader/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss'
+
+function App() {
+
+    return (
+        <>
+            <div className="overlay"></div>
+            <div className="preload">
+                <div className="preload-status">
+                    <div className="preload-status-bar"></div>
+                    <div className="preload-status-info">LOADING</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App;
