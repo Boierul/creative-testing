decc83f6ab3bb37f403238691e4c05fa552d803a
boierul <theboierul@gmail.com>
2023-05-11 22:54:06 +0200

    ...

---
+++ brutalism/brutalism-architecture-mainpage/src/App.jsx
@@ -0,0 +1,52 @@
+import './App.scss'
+
+function App() {
+    // https://codepen.io/redheadedmandy/pen/EbOmrE
+
+    return (
+        <>
+            {/*<div>*/}
+                {/*<nav className="nav navbar navbar-expand-lg justify-content-end">*/}
+                {/*    <button className="navbar-toggler justify-content-end" type="button" data-toggle="collapse"*/}
+                {/*            data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false"*/}
+                {/*            aria-label="Toggle navigation">*/}
+                {/*        <span className="navbar-toggler-icon"></span>*/}
+                {/*    </button>*/}
+
+                {/*    <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">*/}
+                {/*        <a className="nav-link" href="#style">Style</a>*/}
+                {/*        <a className="nav-link" href="#movement">Movement</a>*/}
+                {/*        <a className="nav-link" href="#people">People</a>*/}
+                {/*        <a className="nav-link" href="#legacy">Legacy</a>*/}
+                {/*        <a className="nav-link dropdown-toggle justify-content-end" data-toggle="dropdown"*/}
+                {/*           aria-haspopup="true" aria-expanded="false" href="#">Buildings</a>*/}
+                {/*        <div className="dropdown-menu justify-content-end">*/}
+                {/*            <a className="dropdown-item" href="#habitat">Habitat 67</a>*/}
+                {/*            <a className="dropdown-item" href="#unite">Unite d'Habitation</a>*/}
+                {/*            <a className="dropdown-item" href="#bond">The Bond Center</a>*/}
+                {/*            <a className="dropdown-item" href="#barbican">Barbican Estates</a>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</nav>*/}
+            {/*</div>*/}
+
+            <div className="container-fluid titlecontainer">
+                <div className="row titletext">
+                    <div className="col">
+                        BRUTAL
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <img className="illustration" src="https://images.unsplash.com/photo-1591280122880-eec70c6a5810?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=r-1.2.1&auto=format&fit=crop&w=2882&q=80"/>
+                    </div>
+                    <div className="col titletext">
+                        ISM
+                    </div>
+                </div>
+            </div>
+        </>
+)
+}
+
+export default App;
